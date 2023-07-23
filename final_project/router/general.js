be56642dbde6ca7664c44f727de66d8ccac9a8b3
Anas Jahangir <anasm8931@gmail.com>
2023-07-24 00:43:41 +0500

    final Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,39 +24,106 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  return res.status(200).send(JSON.stringify(books));
+  getAllBooks()
+    .then((Book) => {
+      return res.status(200).send(JSON.stringify(Book));
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).send("Error occurred");
+    });
 });
 
-// Get book details based on ISBN
+
+// BooksByISBN function with Promise callbacks
+function BooksByISBN(ISBN) {
+  return new Promise((resolve, reject) => {
+    try {
+      const book = books[parseInt(ISBN)];
+      if (book) {
+        resolve(book)
+      } else {
+        reject(new Error("No Book By this ISBN"));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// Get book details based on ISBN using Promise callbacks
 public_users.get('/isbn/:isbn', function (req, res) {
   let ISBN = req.params.isbn;
-  let book = books[parseInt(ISBN)];
-  if (book) {
-    return res.status(200).json(book);
-  }
-  return res.status(404).json({ message: "No Book By this ISBN" });
+  BooksByISBN(ISBN)
+    .then((Book) => {
+      return res.status(200).json(Book);
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: error.message });
+    });
 });
 
-// Get book details based on author
+
+// Function to get books by author using Promise callbacks
+function getBooksByAuthor(author) {
+  return new Promise((resolve, reject) => {
+    try {
+      const filterByAuthor = Object.values(books).filter((book) =>
+        (book.author.toLowerCase() === author.toLowerCase()));
+
+      if (filterByAuthor.length > 0) {
+        resolve(filterByAuthor);
+      } else {
+        reject(new Error("No Book By this Author Name"));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// Get book details based on author using Promise callbacks
 public_users.get('/author/:author', function (req, res) {
   let author = req.params.author;
-  let filterByAuthor = Object.values(books).filter((book) => 
-  (book.author.toLowerCase() === author.toLowerCase()))
-  if (filterByAuthor.length > 0) {
-    return res.status(200).json(filterByAuthor);
-  }
-  return res.status(404).json({ message: "No Book By this Author Name" });
+  getBooksByAuthor(author)
+    .then((books) => {
+      return res.status(200).json(books);
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: error.message });
+    });
 });
 
-// Get all books based on title
+// Function to get books by title using Promise callbacks
+function getBooksByTitle(title) {
+  return new Promise((resolve, reject) => {
+    try {
+      const filterByTitle = Object.values(books).filter((book) =>
+        (book.title.toLowerCase() === title.toLowerCase()));
+
+      if (filterByTitle.length > 0) {
+        resolve(filterByTitle);
+      } else {
+        reject(new Error("No Book By this Title"));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// Get all books based on title using Promise callbacks
 public_users.get('/title/:title', function (req, res) {
   let title = req.params.title;
-  let filterByTitle = Object.values(books).filter((book) => (book.title.toLowerCase() === title.toLowerCase()))
-  if (filterByTitle.length > 0) {
-    return res.status(200).json(filterByTitle);
-  }
-  return res.status(404).json({ message: "No Book By this Title" });
+  getBooksByTitle(title)
+    .then((books) => {
+      return res.status(200).json(books);
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: error.message });
+    });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
@@ -67,5 +134,15 @@ public_users.get('/review/:isbn', function (req, res) {
   }
   return res.status(404).json({ message: "No Book By this ISBN" });
 });
+
+function getAllBooks() {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(books);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 module.exports.general = public_users;
